16ca1e9280646f929297d92f8b9c3c0ba1870e01
kdan80 <kierandansey80@gmail.com>
2022-07-04 18:26:04 +0100

    Installed dependencies and updated next.config.js

--- next.config.js
+++ next.config.js
@@ -1,6 +1,13 @@
-/** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  swcMinify: true,
+  i18n: {
+    locales: ['en'],
+    defaultLocale: 'en',
+  },
+  compiler: {
+    styledComponents: true
+  }
 }
 
-module.exports = nextConfig
+module.exports = nextConfig;
