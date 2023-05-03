6947ce83f4bc2078ac3aa82f07be5354c2f214a9
kdan80 <kierandansey80@gmail.com>
2023-05-03 20:46:59 +0100

    Move to laravel

--- components/Tutorial.test.tsx
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import { render } from '@testing-library/react';
-import Tutorial from './Tutorial';
-
-describe("when rendered with `onSendWaves` prop", () => {
-    it("should render the sending waves button", () => {
-        render(<Tutorial/>);
-    });
-  });
