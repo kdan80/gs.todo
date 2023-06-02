0f263a7a2d28bbbfd506bd1682ca4e83749b8346
kdan80 <kierandansey80@gmail.com>
2023-06-02 16:55:09 +0100

    installed react

---
+++ resources/js/app.jsx
@@ -0,0 +1,7 @@
+import './components/Example';
+import ReactDOM from 'react-dom/client';        
+import Example from './components/Example';
+
+ReactDOM.createRoot(document.getElementById('app')).render(     
+    <Example />        
+); 
--- vite.config.js
+++ vite.config.js
@@ -1,22 +1,16 @@
 import { defineConfig } from 'vite';
 import laravel from 'laravel-vite-plugin';
+import react from '@vitejs/plugin-react';
 
 export default defineConfig({
     plugins: [
+        react(),
         laravel({
-            input: ['resources/css/app.css', 'resources/js/app.js'],
+            input: [
+                'resources/js/app.js',
+            ],
             refresh: true,
         }),
-        {
-            name: 'blade',
-            handleHotUpdate({ file, server }) {
-                if (file.endsWith('.blade.php')) {
-                    server.ws.send({
-                        type: 'full-reload',
-                        path: '*',
-                    });
-                }
-            },
-        }
+        react(),
     ],
-});
+});      
