9f31bc160d7638503d33bea83e721dd36c286bd3
kdan80 <kierandansey80@gmail.com>
2023-06-12 18:22:10 +0100

    NASA

--- resources/js/app.jsx
+++
@@ -1,7 +0,0 @@
-import './components/Example';
-import ReactDOM from 'react-dom/client';        
-import Example from './components/Example';
-
-ReactDOM.createRoot(document.getElementById('app')).render(     
-    <Example />        
-); 
