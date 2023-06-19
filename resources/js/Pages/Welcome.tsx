ada0ab3cd6ffcb0cbbf97e43ef93422e56581da8
kdan80 <kierandansey80@gmail.com>
2023-06-19 19:33:06 +0100

    Setup prettier

--- resources/js/Pages/Welcome.tsx
+++ resources/js/Pages/Welcome.tsx
@@ -1,9 +1,9 @@
+/** @format */
+
 import React from 'react'
 
 const Welcome = () => {
-  return (
-    <div className="text-3xl text-red-500">Hello World</div>
-  )
+    return <div className='text-3xl text-red-500'>Hello World</div>
 }
 
 export default Welcome
