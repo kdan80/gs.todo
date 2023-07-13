dcae7e8dd403332af104a7ddd4a8f772ca2842fb
kdan80 <kierandansey80@gmail.com>
2023-07-13 19:25:07 +0100

    Responsive icon

--- resources/js/components/Icon.tsx
+++ resources/js/components/Icon.tsx
@@ -9,9 +9,9 @@ interface Props {
 
 const DottedLine = () => {
     return (
-        <div className='flex flex-row space-x-[4px] items-center'>
-            <span className='h-[5px] w-[5px] bg-white rounded-full'></span>
-            <span className='h-[3px] w-[22px] bg-white rounded-full'></span>
+        <div className='flex flex-row space-x-[4px] md:space-x-[3px] items-center'>
+            <span className='h-[5px] w-[5px] md:h-[4px] md:w-[4px] bg-white rounded-full'></span>
+            <span className='h-[3px] w-[22px] md:h-[2px] md:w-[14px] bg-white rounded-full'></span>
         </div>
     )
 }
@@ -19,9 +19,9 @@ const DottedLine = () => {
 const Icon = ({ color }: Props) => {
     return (
         <div
-            className={`h-12 w-12 ${
+            className={`h-[48px] w-[48px] min-w-[48px] min-h-[48px] md:min-h-[36px] md:min-w-[36px] md:h-[36px] md:w-[36px] ${
                 colorVariants[color as keyof IColorVariants]
-            } rounded-xl flex flex-col items-center gap-[4px] justify-center`}>
+            } rounded-xl flex flex-col items-center gap-[4px] md:gap-[2px] justify-center`}>
             <DottedLine />
             <DottedLine />
             <DottedLine />
