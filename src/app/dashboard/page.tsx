b9c06bbfa13070e02f32c4a1dddd0610a617c129
kdan80 <kierandansey80@gmail.com>
2023-08-03 16:15:52 +0100

    Move to ES6 arrow functions

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -2,8 +2,29 @@
 
 import React from 'react'
 
-const Page = () => {
-    return <div>Dashboard</div>
+const fetchTodoLists = async () => {
+    const res = await fetch(process.env.URL + '/api/todolists', {
+        cache: 'no-store',
+    })
+
+    if (!res.ok) {
+        throw new Error('Failed to fetch TodoLists')
+    }
+    return res.json()
+}
+
+const Page = async () => {
+    const todoLists = await fetchTodoLists()
+    return (
+        <div>
+            Dashboard
+            <ul>
+                {todoLists.map((todoList: any, index: number) => (
+                    <li key={todoList.id}>{todoList.name}</li>
+                ))}
+            </ul>
+        </div>
+    )
 }
 
 export default Page
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,25 +1,14 @@
 /** @format */
 
+import Link from 'next/link'
 import React from 'react'
 
-async function fetchTodoLists() {
-    const res = await fetch(process.env.URL + '/api/todolists', {
-        cache: 'no-store',
-    })
-
-    if (!res.ok) {
-        throw new Error('Failed to fetch TodoLists')
-    }
-    return res.json()
-}
-
-export default async function Page() {
-    const todoLists = await fetchTodoLists()
-    console.log('data', todoLists)
-
+const Page = () => {
     return (
         <main className='flex min-h-screen flex-col items-center justify-between p-24'>
-            Hello world
+            <Link href={'/dashboard'}>Dashboard</Link>
         </main>
     )
 }
+
+export default Page
