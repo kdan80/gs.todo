9a3ffed45bd418c01c5fc24cba59ebad92c6fc0b
kdan80 <kierandansey80@gmail.com>
2023-08-09 15:59:55 +0100

    Use try/catch instead of then

--- prisma/seed.ts
+++ prisma/seed.ts
@@ -4,39 +4,38 @@ import prisma from './client'
 
 const listsToCreate = 10
 const todosToCreate = 10
-
 const colors = ['red', 'teal', 'pink', 'purple', 'yellow', 'blue', 'green']
 
-async function main() {
-    for (let i = 0; i < listsToCreate; i++) {
-        const color = colors[Math.floor(Math.random() * colors.length)]
+const main = async () => {
+    try {
+        for (let i = 0; i < listsToCreate; i++) {
+            const color = colors[Math.floor(Math.random() * colors.length)]
 
-        await prisma.todoList.create({
-            data: {
-                name: `Test Todo List ${i + 1}`,
-                color: color,
-                todos: {
-                    create: Array.from({ length: todosToCreate }).map(
-                        (todo, index) => {
-                            return {
-                                description: `Test Todo ${index + 1}`,
-                                completed: 0,
+            await prisma.todoList.create({
+                data: {
+                    name: `Test Todo List ${i + 1}`,
+                    color: color,
+                    todos: {
+                        create: Array.from({ length: todosToCreate }).map(
+                            (todo, index) => {
+                                return {
+                                    description: `Test Todo ${index + 1}`,
+                                    completed: 0,
+                                }
                             }
-                        }
-                    ),
+                        ),
+                    },
                 },
-            },
-            include: {
-                todos: true,
-            },
-        })
+                include: {
+                    todos: true,
+                },
+            })
+        }
+    } catch (err) {
+        console.log(err)
+    } finally {
+        await prisma.$disconnect()
     }
 }
 
 main()
-    .catch(err => {
-        console.log(err)
-    })
-    .finally(async () => {
-        await prisma.$disconnect()
-    })
