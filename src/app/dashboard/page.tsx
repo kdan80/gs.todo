d6b946d593ed6b3f9d4bc9107c27fd47b023767f
kdan80 <kierandansey80@gmail.com>
2023-08-14 18:56:40 +0100

    Remove dead code

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -36,28 +36,3 @@ const Page = async () => {
 }
 
 export default Page
-
-// const fetchTodoLists = async () => {
-//     const res = await fetch(process.env.URL + '/api/todolists', {
-//         cache: 'no-store',
-//     })
-
-//     if (!res.ok) {
-//         throw new Error('Failed to fetch TodoLists')
-//     }
-//     return res.json()
-// }
-
-// const Page = async () => {
-//     const todoLists = await fetchTodoLists()
-//     return (
-//         <div>
-//             Dashboard
-//             <ul>
-//                 {todoLists.map((todoList: any, index: number) => (
-//                     <li key={todoList.id}>{todoList.name}</li>
-//                 ))}
-//             </ul>
-//         </div>
-//     )
-// }
