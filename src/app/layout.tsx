de84b2a8fc22c0ff6805ec1a5eee6e614ffe8a6f
kdan80 <kierandansey80@gmail.com>
2023-08-23 19:48:24 +0100

    Added manifest.json

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -8,6 +8,8 @@ const inter = Inter({ subsets: ['latin'] })
 export const metadata: Metadata = {
     title: 'gs.todo',
     description: "If you've got sh!t to do, you've got gs.todo",
+    manifest: '/manifest.json',
+    themeColor: '#20212C',
 }
 
 export default function RootLayout({
